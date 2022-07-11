724134116511720394e3ccd0e23ae26f1edfa8dd
luis08201 <luis08201@gmail.com>
2022-07-11 13:25:31 -0600

    adding animation to burger btn

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,6 +1,18 @@
 let burgerBtn = document.getElementById("burgerBtn");
 
 burgerBtn.addEventListener("click", () =>{
+    burgerBtn.classList.toggle("active");
     let nav = document.getElementById("nav");
     nav.classList.toggle("show");
+
+    let bars = document.querySelectorAll(".header__top--bar");
+
+    bars.forEach(bar => {
+        if (burgerBtn.classList.contains("active")) {
+            bar.classList.add("active");
+        } else {
+            bar.classList.remove("active");
+        }
+    })
+    
 })
